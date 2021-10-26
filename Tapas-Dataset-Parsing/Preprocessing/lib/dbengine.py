218c5c14f5093b95764805a418d5459c9382eb07
Matagi <dev_matagi@kakao.com>
2021-10-26 19:47:59 +0900

    ADD, Citation

--- Tapas-Dataset-Parsing/Preprocessing/lib/dbengine.py
+++ Tapas-Dataset-Parsing/Preprocessing/lib/dbengine.py
@@ -1,3 +1,16 @@
+'''
+@article{zhongSeq2SQL2017,
+  author    = {Victor Zhong and
+               Caiming Xiong and
+               Richard Socher},
+  title     = {Seq2SQL: Generating Structured Queries from Natural Language using
+               Reinforcement Learning},
+  journal   = {CoRR},
+  volume    = {abs/1709.00103},
+  year      = {2017}
+}
+'''
+
 import records
 import re
 from babel.numbers import parse_decimal, NumberFormatError
