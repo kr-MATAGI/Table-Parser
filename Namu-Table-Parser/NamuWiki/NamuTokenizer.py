4b18f864dc0db0f3a2ff77ec58204864b3ca40ce
Matagi <dev_matagi@kakao.com>
2021-10-26 18:34:26 +0900

    Merge Data Parsing code

---
+++ Namu-Table-Parser/NamuWiki/NamuTokenizer.py
@@ -0,0 +1,62 @@
+### compute module
+import numpy as np
+
+### Hugging Face - transformer
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+# Tokenizer
+tokenizer = AutoTokenizer.from_pretrained("klue/roberta-base")
+
+
+class NamuTokenizer:
+    ### VAR ###
+
+    ### PRIVATE ###
+
+    ### PUBLIC ###
+    '''
+        @Note
+            Save Data ?
+        @Param
+            srcDataList:[
+                0: sequence_has_ans
+                1: segments_has_ans
+                2: masks_has_ans
+                3: rows_has_ans
+                4: cols_has_ans
+                5: label_ids
+                6: label_position
+                7: label_weight
+            ]
+    '''
+    def SaveTensorDataSet(self, count, maxLen, maxMasking, *srcDataList):
+        sequence_has_ans_ = np.zeros(shape=[count, maxLen], dtype=np.int32)
+        segments_has_ans_ = np.zeros(shape=[count, maxLen], dtype=np.int32)
+        masks_has_ans_ = np.zeros(shape=[count, maxLen], dtype=np.int32)
+        cols_has_ans_ = np.zeros(shape=[count, maxLen], dtype=np.int32)
+        rows_has_ans_ = np.zeros(shape=[count, maxLen], dtype=np.int32)
+
+        label_ids_ = np.zeros(shape=[count, maxMasking], dtype=np.int32)
+        label_position_ = np.zeros(shape=[count, maxMasking], dtype=np.int32)
+        label_weight_ = np.zeros(shape=[count, maxMasking], dtype=np.float32)
+
+        for idx in range(count):
+            sequence_has_ans_[idx] = srcDataList[0][idx]
+            segments_has_ans_[idx] = srcDataList[1][idx]
+            masks_has_ans_[idx] = srcDataList[2][idx]
+            rows_has_ans_[idx] = srcDataList[3][idx]
+            cols_has_ans_[idx] = srcDataList[4][idx]
+
+            label_ids_[idx] = srcDataList[5][idx]
+            label_position_[idx] = srcDataList[6][idx]
+            label_weight_[idx] = srcDataList[7][idx]
+
+        np.save('sequence_table_pre3', sequence_has_ans_)
+        np.save('segments_table_pre3', segments_has_ans_)
+        np.save('masks_table_pre3', masks_has_ans_)
+        np.save('rows_table_pre3', rows_has_ans_)
+        np.save('cols_table_pre3', cols_has_ans_)
+
+        np.save('label_ids3', label_ids_)
+        np.save('label_position3', label_position_)
+        np.save('label_weight3', label_weight_)
---
+++ Namu-Table-Parser/PreProcesserMain.py
@@ -0,0 +1,72 @@
+from NamuWiki.NamuParser import DOC_TEXT, DOC_TITLE, NamuWikiParser
+from HeadExtractor import TableHeaderExtractor
+
+# Definition
+SRC_JSON_PATH = './dataset/docData200302.json'
+WRITE_TABLE_DEST_PATH = './Tables'
+
+# TEST
+TEST_TAGET_TITLE = '백 평짜리 숲(킹덤 하츠)'
+TEST_MODE = True
+
+from eunjeon import Mecab
+
+if __name__ == '__main__':
+
+    # eunjeon.Mecab Test
+    #mecab = Mecab()
+    #text = "이모 여기 뚝배기 하나 추가요~!"
+    #a = mecab.pos(text)
+    #print(a)
+    #exit()
+
+    # Make instance    
+    namuParser = NamuWikiParser(SRC_JSON_PATH)
+    headExtractor = TableHeaderExtractor()
+
+    docCount = 0
+    for docItem in namuParser.ParsingJSON():
+        docCount += 1
+
+        if 0 == docCount % 1000:
+            print('Processing....', docItem[DOC_TITLE], docCount)
+
+        #### TEST ####
+        if TEST_MODE and TEST_TAGET_TITLE != docItem[DOC_TITLE]: continue
+
+        tableList = namuParser.ParseTableFromText(docItem[DOC_TEXT])
+
+        #### TEST ###
+        #if TEST_MODE and TEST_TAGET_TITLE == docItem[DOC_TITLE]:
+            #retTestList = namuParser.ParseTableAndDetailsFromDocument(docItem[DOC_TITLE], docItem[DOC_TEXT])
+            #print(retTestList)
+            #break
+
+        if 0 < len(tableList):
+            newTableList = []
+            modifiedTableList = namuParser.ModifyHTMLTags(tableList)
+            break
+
+            for table in modifiedTableList:
+                preprocessedTable = namuParser.PreprocessingTable(table)
+
+                if 2 <= len(preprocessedTable):
+                    newTableList.append(preprocessedTable)
+            normalTableList, infoBoxList = namuParser.ClassifyNormalTableOrInfoBox(newTableList)
+
+            # Write Table to Directory
+            #namuParser.WriteTableToFile(normalTableList, docItem[DOC_TITLE], WRITE_TABLE_DEST_PATH+'/normal', True)
+            #namuParser.WriteTableToFile(infoBoxList, docItem[DOC_TITLE], WRITE_TABLE_DEST_PATH+'/infobox', False)
+
+            scoreTable = headExtractor.GiveScoreToHeadCell(normalTableList)
+            #### TEST ####
+            if TEST_MODE and TEST_TAGET_TITLE == docItem[DOC_TITLE]:
+                print(normalTableList)
+                print(infoBoxList)
+                print(scoreTable)
+
+        #### TEST ####
+        if TEST_MODE and TEST_TAGET_TITLE == docItem[DOC_TITLE]:
+            print("NOT EXISTED TABLE")
+            # print(docItem[DOC_TEXT])
+            break
