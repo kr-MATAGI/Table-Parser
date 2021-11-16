c096abdb344ebc077a5a02910c980dfe9a11d26d
Matagi <dev_matagi@kakao.com>
2021-11-16 20:52:59 +0900

    add, ref url

--- Deep-learning-Project/Model.py
+++ Deep-learning-Project/Model.py
@@ -1,6 +1,11 @@
 from transformers import RobertaConfig, RobertaModel
 
 
+'''
+    How to pretrained RoBERTa
+    ref: https://github.com/pytorch/fairseq/blob/main/examples/roberta/README.pretraining.md
+    https://www.reddit.com/r/LanguageTechnology/comments/gyxwz3/how_to_further_pretrain_bert_or_roberta_model/
+'''
 
 if "__main__" == __name__:
     # Init a RoBERTa configuration
