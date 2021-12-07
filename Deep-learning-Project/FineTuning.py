91aca1ed000697503fd275f4095cfffe97982107
kr-MATAGI <dev_matagi@kakao.com>
2021-12-07 21:00:29 +0900

    add, fine tuning loss

--- Deep-learning-Project/FineTuning.py
+++ Deep-learning-Project/FineTuning.py
@@ -20,11 +20,20 @@ class KorQuadDataset(datasets.Dataset):
     def __getitem__(self, idx):
         data = self.srcData[idx]
         items = {
-            "labels": torch.tensor(data["answers"]),
+            "labels": None,
             "input_ids": None,
             # "attention_mask": torch.tensor(np.zeros(512)),
             "token_type_ids": None
         }
+
+        ## labels
+        startIdx = data["labels"][0]
+        endIdx = data["labels"][1]
+        label = data["input_ids"][0][startIdx:endIdx]
+
+        empty_tensor = torch.tensor(np.zeros(512 - len(label)), dtype=torch.int64)
+        label_tensor = torch.tensor(label, dtype=torch.int64)
+        items["labels"] = torch.concat([label_tensor, empty_tensor], dim=-1)
 
         ## input_ids
         items["input_ids"] = torch.tensor(data["input_ids"][0], dtype=torch.int64)
@@ -77,50 +86,50 @@ if "__main__" == __name__:
 
     # My Custom
     data_num = answer_span_table_npy.shape[0]
-    pre_labels = torch.tensor(np.zeros([data_num, 512]))
     print("Complete - Load Npy Files")
 
     # Datasets
-    train_last_idx = int(data_num * 0.8)
+    data_split_idx = int(data_num * 0.8)
+    print("Total Data Size:", data_num, "Data split Index:", data_split_idx)
 
     train_data_dict = {
-        "answers": [],
+        "labels": [],
         "input_ids": [],
         "segment_ids": [],
         "column_ids": [],
         "row_ids": [],
         # "column_ranks": [],
     }
-    train_data_dict["answers"] = answer_span_table_npy[:train_last_idx]
-    train_data_dict["input_ids"] = sequence_table_npy[:train_last_idx]
-    train_data_dict["segment_ids"] = segments_table_npy[:train_last_idx]
-    train_data_dict["column_ids"] = cols_table_npy[:train_last_idx]
-    train_data_dict["row_ids"] = rows_table_npy[:train_last_idx]
+    train_data_dict["labels"] = answer_span_table_npy[:data_split_idx]
+    train_data_dict["input_ids"] = sequence_table_npy[:data_split_idx]
+    train_data_dict["segment_ids"] = segments_table_npy[:data_split_idx]
+    train_data_dict["column_ids"] = cols_table_npy[:data_split_idx]
+    train_data_dict["row_ids"] = rows_table_npy[:data_split_idx]
     # train_data_dict["column_ranks"]
 
     train_dataset = datasets.Dataset.from_dict(train_data_dict)
     train_dataset = KorQuadDataset(train_dataset)
+    print("Train Datasets Size:", len(train_dataset))
     train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2)
 
-    batch = next(iter(train_data_loader))
-
     test_data_dict = {
-        "answers": [],
+        "labels": [],
         "input_ids": [],
         "segment_ids": [],
         "column_ids": [],
         "row_ids": [],
         # "column_ranks": [],
     }
-    test_data_dict["answers"] = answer_span_table_npy[train_last_idx:]
-    test_data_dict["input_ids"] = sequence_table_npy[train_last_idx:]
-    test_data_dict["segment_ids"] = segments_table_npy[train_last_idx:]
-    test_data_dict["column_ids"] = cols_table_npy[train_last_idx:]
-    test_data_dict["row_ids"] = rows_table_npy[train_last_idx:]
+    test_data_dict["labels"] = answer_span_table_npy[data_split_idx:-1]
+    test_data_dict["input_ids"] = sequence_table_npy[data_split_idx:-1]
+    test_data_dict["segment_ids"] = segments_table_npy[data_split_idx:-1]
+    test_data_dict["column_ids"] = cols_table_npy[data_split_idx:-1]
+    test_data_dict["row_ids"] = rows_table_npy[data_split_idx:-1]
     # test_data_dict["column_ranks"]
 
     test_dataset = datasets.Dataset.from_dict(test_data_dict)
     test_dataset = KorQuadDataset(test_data_dict)
+    print("Test Datasets Size:", len(test_dataset))
     test_data_loader = torch.utils.data.DataLoader(test_data_dict, batch_size=2)
 
     # Fine tuning
@@ -138,7 +147,7 @@ if "__main__" == __name__:
             input_ids = batch["input_ids"].to(device)
             # attention_mask = batch["attention_mask"].to(device)
             token_type_ids = batch["token_type_ids"].to(device)
-            labels = batch["answers"].to(device) # shape (batch_size, 2)
+            labels = batch["labels"].to(device) # shape (batch_size, 2)
 
             # Test - Check Shape
             # print("input_ids.shape", input_ids.shape)
@@ -148,7 +157,8 @@ if "__main__" == __name__:
             optimizer.zero_grad()
 
             outputs = model(input_ids=input_ids,
-                            token_type_ids=token_type_ids)
+                            token_type_ids=token_type_ids,
+                            labels=labels)
 
             loss = outputs.loss
             print("Loss:", loss.item()) # loss == None
